3cf3415870b0984530df2d2f89ec6465090badac
Maximilian Barth <maximilian1barth@gmail.com>
2024-04-05 22:54:46 +0200

    added sqlite

--- web/cmd/main.go
+++ web/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"io"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/major-seven/thisinformation/web/article"
+  _ "github.com/mattn/go-sqlite3"
 )
 
 type Template struct {
@@ -37,6 +39,32 @@ func main() {
 		panic("NEWS_KEY is not set")
 	}
 
+  db, err := sql.Open("sqlite3", "./news.db")
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+
+  _, err = db.Exec("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT)")
+  if err != nil {
+    panic(err)
+  }
+
+  rows, err := db.Query("SELECT title, content FROM articles")
+  if err != nil {
+    panic(err)
+  }
+  defer rows.Close()
+
+  for rows.Next() {
+    a := new(article.Article)
+    err = rows.Scan(&a.Title, &a.Content)
+    if err != nil {
+      panic(err)
+    }
+    data.Articles = append(data.Articles, a)
+  }
+
 	app := echo.New()
 	app.Renderer = newTemplate()
 	app.Use(middleware.Logger())
@@ -55,6 +83,11 @@ func main() {
 
       if a.Title == "" || a.Content == "" {
         return c.String(http.StatusBadRequest, "bad request")
+      }
+
+      _, err := db.Exec("INSERT INTO articles (title, content) VALUES (?, ?)", a.Title, a.Content)
+      if err != nil {
+        return c.String(http.StatusInternalServerError, "internal server error")
       }
 
 			data.Articles = append(data.Articles, a)
@@ -82,6 +115,13 @@ func main() {
         }
       }
 
+      for _, a := range articles.Articles {
+        _, err := db.Exec("INSERT INTO articles (title, content) VALUES (?, ?)", a.Title, a.Content)
+        if err != nil {
+          return c.String(http.StatusInternalServerError, "internal server error")
+        }
+      }
+
       data.Articles = append(data.Articles, articles.Articles...)
       return c.String(http.StatusOK, "ok")
 
@@ -93,6 +133,11 @@ func main() {
 	app.DELETE("/api/delete-articles/:key", func(c echo.Context) error {
 		if c.Param("key") == KEY {
 			data.Articles = nil
+      _, err := db.Exec("DELETE FROM articles")
+      if err != nil {
+        return c.String(http.StatusInternalServerError, "internal server error")
+      }
+
 			return c.String(http.StatusOK, "ok")
 		} else {
 			return c.String(http.StatusUnauthorized, "unauthorized")
